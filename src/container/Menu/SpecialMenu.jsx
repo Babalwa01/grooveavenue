73834c960884ae8108ac8fc1a070daf5c50c4f75
Babalwa Mbolekwa <babalwa.mbolekwa@clearscore.com>
2022-10-06 21:18:38 +0200

    added special menu section

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -4,18 +4,15 @@ import { images, data } from "../../constants";
 import "./SpecialMenu.css";
 
 const SpecialMenu = () => (
-  <div
-    className="app__specialMenu flex__center section section__padding"
-    id="menu"
-  >
+  <div className="app__specialMenu flex__center section__padding" id="menu">
     <div className="app__specialMenu-title">
-      <SubHeading title="Menu that fits you palatte" />
+      <SubHeading title="Menu that fits your palatte" />
       <h1 className="headtext__cormorant">Today's Special</h1>
     </div>
     <div className="app__specialMenu-menu">
       <div className="app__specialMenu-menu_wine flex__center">
         <p className="app__specialMenu-menu_heading">Wine & Beer</p>
-        <div className="app__specialMenu-menu_items">
+        <div className="app__specialMenu_menu_items">
           {data.wines.map((wine, index) => (
             <MenuItem
               key={wine.title + index}
@@ -32,8 +29,8 @@ const SpecialMenu = () => (
       </div>
 
       <div className="app__specialMenu-menu_cocktails flex__center">
-        <p className="app__specialMenu-menu_heading">Cocktails</p>
-        <div className="app__specialMenu-menu_items">
+        <p className="app__specialMenu_menu_heading">Cocktails</p>
+        <div className="app__specialMenu_menu_items">
           {data.cocktails.map((cocktail, index) => (
             <MenuItem
               key={cocktail.title + index}
